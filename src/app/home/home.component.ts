e44df1aeecc1ea721e65ed009fc16c7d47b01daf
mywebcreations <mywebcreativities@gmail.com>
2022-05-30 19:02:23 +0200

    1 binaries uploaded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit, OnDestroy {
-  
+
 
   ngOnInit(): void {
-
   }
 
   ngOnDestroy(): void {
-
   }
 }
