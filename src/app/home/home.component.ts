838b6a92d15877d8132e48e9966b1acfaa211e6b
mywebcreations <mywebcreativities@gmail.com>
2022-05-31 11:41:20 +0200

    Completed home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
-// import { }
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   event = {
-    title: "COME TO CELEBRATE THE NEW YEAR WITH US!",
-    logo: "../assets/company-logo.png",
-    companyName: "IT COMPANY",
-    tagline: "Implementing your IT solutions",
-    message: "We will be more than happy to",
-    messageContd: "have you with us for this very",
-    dayOfWeek: "Wednesday",
-    month: "December",
+    title: 'COME TO CELEBRATE THE NEW YEAR WITH US!',
+    logo: '../assets/company-logo.png',
+    companyName: 'IT COMPANY',
+    tagline: 'Implementing your IT solutions',
+    message: 'We will be more than happy to',
+    messageContd: 'have you with us for this very',
+    dayOfWeek: 'Wednesday',
+    month: 'December',
     day: 29,
-    timePeriod: "19h to 23h",
-    action: "ACCEPT/REFUSE"
-  }
+    timePeriod: '19h to 23h',
+    action: 'ACCEPT/REFUSE',
+  };
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   processClick(): void {
-    this.router.navigate(['/register'])
+    this.router.navigate(['/register']);
   }
-
 }
