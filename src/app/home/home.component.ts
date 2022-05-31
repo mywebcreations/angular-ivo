562022225e12fb10b2b7036cdeae3fccaec96f79
mywebcreations <mywebcreativities@gmail.com>
2022-05-31 11:35:32 +0200

    created core resources

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+// import { }
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+export class HomeComponent implements OnInit {
 
-export class HomeComponent implements OnInit, OnDestroy {
+  event = {
+    title: "COME TO CELEBRATE THE NEW YEAR WITH US!",
+    logo: "../assets/company-logo.png",
+    companyName: "IT COMPANY",
+    tagline: "Implementing your IT solutions",
+    message: "We will be more than happy to",
+    messageContd: "have you with us for this very",
+    dayOfWeek: "Wednesday",
+    month: "December",
+    day: 29,
+    timePeriod: "19h to 23h",
+    action: "ACCEPT/REFUSE"
+  }
 
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    
   }
 
-  ngOnDestroy(): void {
+  processClick(): void {
+    this.router.navigate(['/register'])
   }
+
 }
