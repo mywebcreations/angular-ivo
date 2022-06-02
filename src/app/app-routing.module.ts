f17bf8891fe9056e32a217e353663a14c8d405f6
mywebcreations <mywebcreativities@gmail.com>
2022-06-02 11:49:20 +0200

    Refactoring: added EditRegistrationComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditRegistrationComponent } from './edit-registration/edit-registration.component';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 
@@ -7,12 +8,12 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegistrationComponent },
-  
+  { path: 'edit-registration/:id', component: EditRegistrationComponent }  
   
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
